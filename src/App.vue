cc8fafd82ba0f6844f7f9d01f903dd881f8f5349
Thiago Kasper de Souza <tsouza@parks.com.br>
2023-08-22 15:25:43 -0300

    Initial commit - grafana snapshot embedding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://www.parks.com.br/quem-somos" target="_blank">
+      <img src="/parks.png" class="logo" alt="Vite logo" style="border-radius: 8px;" />
+    </a>
+  </div>
+  <HelloWorld msg="Parks Smart" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #ff6964aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <p>
+    AWS IoT Core with C++ Device SDK, Rules-Engine, Timestream and Grafana example
+  </p>
+  <h1>Grafana Dashboard - ESP32 with DHT22</h1>
+  <iframe class="iframe"
+    src="https://snapshots.raintank.io/dashboard/snapshot/4ozI8XVcO6hzW8jFlhqwZkHWqX1r6nR5?orgId=2&refresh=5s&kiosk"
+    frameborder="0"></iframe>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+
+.frame-wrapper {
+  position: relative;
+  width: 700px;
+  height: 80%;
+  overflow-y: hidden;
+  overflow-x: scroll;
+}
+
+.iframe {
+  width: 80vw;
+  height: 625px;
+}
+</style>
